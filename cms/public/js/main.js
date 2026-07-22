98cc10676f8458ca7f9d5b293ba7f90e5ed79cac
Lawrence Auguste <lawrence_auguste@yahoo.com>
2026-07-22 13:26:49 +0500

    Use public admin login path

--- cms/public/js/main.js
+++ cms/public/js/main.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /* ---------- Client + admin login navigation ---------- */
 function initLoginNavigation() {
-  const dashboardUrl = 'admin-login.html';
+  const dashboardUrl = 'https://positivelegacysolutions.com/admin-login';
   const desktopNav = document.querySelector('.site-header nav');
 
   if (desktopNav && !desktopNav.querySelector('[data-login-menu]')) {
